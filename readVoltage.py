05c26f25d8c23892bb7a0c94da9a3d434228f291
leaf <krissthompson@protonmail.com>
2022-05-30 19:22:45 +0200

    remove redundant files

---
+++ readVoltage.py
@@ -0,0 +1,35 @@
+#
+# A.Skoczen, AGH0-UST
+# Created: 04.2021
+# multimetr agilent 34401a -> RS-232 -> FTDI -> USB -> pyftdi
+# przyklad poprawnego rozwiazania pracy sygnalow DSR, DTR
+# Developed: 31/05.2021
+#
+import pyftdi.serialext
+import time
+import os, sys, platform
+import struct
+import readVoltageLib
+
+
+readVoltageLib.assertLinux()
+readVoltageLib. assertPython3()
+
+port = pyftdi.serialext.serial_for_url(url='ftdi://ftdi:232:FT4T6R2Q/1', baudrate=9600, timeout=10, bytesize=8, stopbits=2, parity='N')
+#, xonxoff=False, rtscts=False, dsrdtr=True)
+readVoltageLib.initial(port)
+
+while True :
+    userInput = input("digit to start, letter to end")
+    print("You entered: ", userInput)
+    if not userInput.isdigit():
+        print('ending...')
+        break;
+    print('starting...')
+    voltageReceived = readVoltageLib.readAverageVoltage(port)
+    print(voltageReceived)
+
+remote = "SYST:LOC\n"
+port.write(remote)
+"""
+"""
--- readVoltageLib.py
+++ readVoltageLib.py
@@ -1,14 +1,7 @@
-#
-# A.Skoczen, AGH0-UST
-# Created: 04.2021
-# multimetr agilent 34401a -> RS-232 -> FTDI -> USB -> pyftdi
-# przyklad poprawnego rozwiazania pracy sygnalow DSR, DTR
-# Developed: 31/05.2021
-#
 import pyftdi.serialext
 import time
 import os, sys, platform
-import struct
+import struct  
 
 def assertLinux():
     #check platform :
@@ -27,7 +20,7 @@ def assertPython3():
         print("Wrong python version")
         sys.exit()
 
-def initial(port):
+def initial(port, debug=False):
     port.write(b'\x03')
     time.sleep(1)
     if debug : print("Initial DSR: ", port.dsr)
@@ -61,7 +54,14 @@ def initial(port):
     samp = "SAMP:COUN 1\n"
     port.write(samp)
 
-def readVoltage(port):
+def readAverageVoltage(port, debug = False, seconds = 0.5):
+    avg = 0.0
+    sampleAmount = seconds/0.05
+    for i in range( int(sampleAmount)):
+        avg += readVoltage(port, debug)
+    return avg / sampleAmount 
+
+def readVoltage(port, debug=False):
     #time.sleep(1)
     if debug : print("DSR befor read:", port.dsr)
     port.write("READ?\n")
@@ -73,41 +73,9 @@ def readVoltage(port):
     # Important !!!!!!
     port.dtr = True
     data = port.read(2024)
-    time.sleep(1)
+    time.sleep(0.05)
     if data[-1] != '\n' : 
         data = data + port.read(2024)
     port.dtr = False
     if debug : print('-', data)
     return float(data)
-
-assertLinux()
-assertPython3()
-
-debug = True   
-data = "HEWLETT-PACARD HEWLETT-PACARD HEWLETT-PACARD "
-brate = 9600
-url = 'ftdi://ftdi:232:FT4T6R2Q/1'
-port = pyftdi.serialext.serial_for_url(url, baudrate=brate, timeout=10, bytesize=8, stopbits=2, parity='N')
-#, xonxoff=False, rtscts=False, dsrdtr=True)
-LSB = 3.3/2**8
-initial(port)
-
-next = True
-while next :
-    userInput = input("digit to start, letter to end")
-    print("You entered: ", userInput)
-    if not userInput.isdigit():
-        print('ending...')
-        next = False
-        continue;
-    print('starting...')
-    voltageReceived = readVoltage(port)
-    print(voltageReceived)
-
-remote = "SYST:LOC\n"
-port.write(remote)
-"""
-"""
-
-
-# return error code -101 = invalid character
