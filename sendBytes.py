8549143c4ac394da010fa4bba8f72d9ba01c6444
OneChoosen <84570743+OneChoosen@users.noreply.github.com>
2022-06-15 17:20:12 +0200

    python v1.1

--- sendBytes.py
+++ sendBytes.py
@@ -11,17 +11,31 @@ brate = 230400
 #shockley
 #url  = 'ftdi://ftdi:232:AB0K3Q4S/1'
 #UBUNTU at home
-url = 'ftdi://ftdi:232:AB0K3Q4S/1'
+url = 'ftdi://ftdi:232:AQ00RVND/1'
 port = pyftdi.serialext.serial_for_url(url, baudrate=brate, bytesize=8, stopbits=1, parity='N', xonxoff=False, rtscts=False)
+
+
+DAC_resolution = 2**12 -1 #4095
+
+def voltageToBytesArray(voltage):
+    voltageInDAC_resulution = int((voltage/2.5) * DAC_resolution)
+#    data = voltageInDAC_resulution.to_bytes(2, 'little')
+    first_byte = voltageInDAC_resulution.to_bytes(2, 'little')[1]
+    second_byte =  voltageInDAC_resulution.to_bytes(2, 'little')[0]
+    print(f'data= ', data)
+    return bytes([0x80 + len(data), first_byte, second_byte, 0x80])
+
+
 
 # Send bytes
 print("Transmition at", brate)
 #data = [0x32, 0x4c, 0x31, 0x36, 0x35, 0x37]
-dac_value = 4023
+dac_value = 723
 data = dac_value.to_bytes(2, 'big')
 print(f'data= ', data)
 fullLength = 0xc0 + len(data)
-b = bytes([0x33, 0x35, 0x80 + len(data), *data,  0x80, fullLength])
+#b = bytes([0x80 + len(data), *data,  0x80, fullLength])
+b = voltageToBytesArray(2.11)
 print("-", b)
 port.write(b)
 
@@ -29,7 +43,7 @@ port.write(b)
 nb = len(data)
 print("Receiving at", brate)
 print(nb, "bytes")
-data = port.read(nb)
+#data = port.read(nb)
 print('-', data)
 
 #
