c733d8e60a84d49da1bd9b12c8abcf7b875568c7
leaf <krissthompson@protonmail.com>
2022-06-15 19:02:38 +0200

    remove old projects

--- sendBytes.py
+++ sendBytes.py
@@ -1,49 +1,47 @@
 # Enable pyserial extensions
 import pyftdi.serialext
+from bitstring import BitArray
 
-
-count = 0
-# Open a serial port on the second FTDI device interface (IF/2) @ 3Mbaud
 brate = 230400
-#115200
-#9600
-#230400
-#shockley
-#url  = 'ftdi://ftdi:232:AB0K3Q4S/1'
-#UBUNTU at home
+
 url = 'ftdi://ftdi:232:AQ00RVND/1'
-port = pyftdi.serialext.serial_for_url(url, baudrate=brate, bytesize=8, stopbits=1, parity='N', xonxoff=False, rtscts=False)
+# port = pyftdi.serialext.serial_for_url(url, baudrate=brate, bytesize=8, stopbits=1, parity='N', xonxoff=False, rtscts=False)
 
 
 DAC_resolution = 2**12 -1 #4095
 
 def voltageToBytesArray(voltage):
     voltageInDAC_resulution = int((voltage/2.5) * DAC_resolution)
-#    data = voltageInDAC_resulution.to_bytes(2, 'little')
-    first_byte = voltageInDAC_resulution.to_bytes(2, 'little')[1]
-    second_byte =  voltageInDAC_resulution.to_bytes(2, 'little')[0]
-    print(f'data= ', data)
-    return bytes([0x80 + len(data), first_byte, second_byte, 0x80])
+    data = voltageInDAC_resulution.to_bytes(2, 'little')
+
+    print(f'{voltage=}')
+    print(f'{voltageInDAC_resulution=}')
+    print(f'{data}\t\thex')
+
+    bigEndianHex = hex(voltageInDAC_resulution)
+    print(f'{bigEndianHex}\t\tbigEndianHex')
+    num = voltageInDAC_resulution
+    littleEndianBits = [(num >> shift_ind) & 1
+             for shift_ind in range(num.bit_length())] # little endian
+    bigEndianBits = list(reversed(littleEndianBits)) # big endian
+    print(f'{littleEndianBits}\t\tlittleEndian')
+    print(f'{bigEndianBits}\t\bigEndian')
+
+    return bytes([0x80 + len(data), data[1], data[0], 0x80])
+
+print("Transmition:")
 
 
-
-# Send bytes
-print("Transmition at", brate)
-#data = [0x32, 0x4c, 0x31, 0x36, 0x35, 0x37]
-dac_value = 723
-data = dac_value.to_bytes(2, 'big')
-print(f'data= ', data)
-fullLength = 0xc0 + len(data)
-#b = bytes([0x80 + len(data), *data,  0x80, fullLength])
-b = voltageToBytesArray(2.11)
+voltage = 2.1
+b = voltageToBytesArray(voltage)
 print("-", b)
-port.write(b)
+# port.write(b)
 
-# Receive bytes
-nb = len(data)
-print("Receiving at", brate)
-print(nb, "bytes")
-#data = port.read(nb)
-print('-', data)
+# # Receive bytes
+# nb = len(data)
+# print("receiving")
+# print(nb, "bytes")
+# #data = port.read(nb)
+# print('-', data)
 
-#
+# #
